b52030833cb2aa5cc4fe22623b408887251df2fe
Mereyda <mereyda2487@gmail.com>
2023-02-21 11:54:09 -0300

    Primera entrega Bootcamp

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        int dato1 = 10;
+        long dato2 = 150;
+        double dato3 = 1.5;
+        boolean dato4 = true;
+        String dato5 = "Hola BootCamp";
+
+        System.out.println(dato1);
+        System.out.println(dato2);
+        System.out.println(dato3);
+        System.out.println(dato4);
+        System.out.println(dato5);
+
+    }
+}
